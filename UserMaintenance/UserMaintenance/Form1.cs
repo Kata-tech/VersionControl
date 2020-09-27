c651209a4905c79e1b74a5e5b12e3983129b7359
Kata Gáspár <kata.gaspar2@stud.uni-corvinus.hu>
2020-09-27 20:14:02 +0200

    Commit 7th step

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -19,8 +19,7 @@ namespace UserMaintenance
         public Form1()
         {
             InitializeComponent();
-            label1.Text = Resource1.LastName;
-            label2.Text = Resource1.FirstName;
+            label1.Text = Resource1.FullName;
             button1.Text = Resource1.Add;
 
             listBox1.DataSource = users;
@@ -32,8 +31,8 @@ namespace UserMaintenance
         {
             var use = new User()
             {
-                LastName = label1.Text,
-                FirstName = label2.Text,
+                FullName = textBox1.Text,
+                
             };
             users.Add(use);
         }
