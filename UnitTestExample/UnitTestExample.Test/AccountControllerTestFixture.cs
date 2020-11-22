b7af60dbbcf0c4a48622ddf9ec95903b0dc23c21
Kata Gáspár <kata.gaspar2@stud.uni-corvinus.hu>
2020-11-22 18:18:49 +0100

    Commit 3A

--- UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -4,16 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using NUnit.Framework;
+using UnitTestExample.Controllers;
 
 namespace UnitTestExample.Test
 {
-    class AccountControllerTestFixture
+    
+    public class AccountControllerTestFixture
     {
         [Test]
         public void TestValidateEmail(string email, bool expectedResult)
         {
+            // Arrange
+            var accountController = new AccountController();
 
+            // Act
+            var actualResult = accountController.ValidateEmail(email);
+
+            // Assert
+            Assert.AreEqual(expectedResult, actualResult);
         }
+
+        
+        
     }
 
     
